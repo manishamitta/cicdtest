cf38ef7456c181d885e548368c06c8a0310bc1b4
manisha mitta <mitta.manisha@peolsolutions.com>
2025-05-09 10:23:10 +0000

    commit1

---
+++ app/firstapp/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'firstapp/test/integration/FirstJourney',
+		'firstapp/test/integration/pages/BooksList',
+		'firstapp/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('firstapp') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
---
+++ app/services.cds
@@ -0,0 +1,2 @@
+
+using from './firstapp/annotations';
